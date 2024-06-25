b4a41bbb8511aa1a37b36d6679b0f1e82764b8b5
KingVentrix007 <tristanjkuhn007@gmail.com>
2024-06-25 09:39:07 +0200

    minor changes

--- scripts/athenxHost.sh
+++ scripts/athenxHost.sh
@@ -1,6 +1,6 @@
 image_file="AthenX.img"
 sudo losetup /dev/loop1 "$image_file" -o 1048576
 sudo mount /dev/loop1 /mnt/AthenX-3.0
-sudo cp -r /mnt/AthenX-3.0/. testD/.
+sudo cp -r /mnt/AthenX-3.0/. drive_output/.
 sudo umount /mnt/AthenX-3.0
 sudo losetup -d /dev/loop1
